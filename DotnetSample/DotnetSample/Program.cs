6a07270e590eff4d32d5cc71f0ce9420fb9b2ebe
Thiago Silva <thiagoddsilva@gmail.com>
2024-03-01 17:21:37 -0300

    first change

--- DotnetSample/DotnetSample/Program.cs
+++ DotnetSample/DotnetSample/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+
+Console.WriteLine("Hello, World!!");
+
+var fileRead = File.OpenRead("test.txt");
+byte[] b = new byte[1024];
+UTF8Encoding temp = new UTF8Encoding(true);
+
+while (fileRead.Read(b,0,b.Length) > 0)
+{
+    Console.WriteLine(temp.GetString(b));
+}
